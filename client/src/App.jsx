f0b4bd0fcd67defe54152defccc1e12ec6481984
Rajas Mhatre <rajasmhatre2704@gmail.com>
2025-02-05 20:42:09 +0530

    changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,21 +1,24 @@
 import React from 'react'
 import {BrowserRouter , Routes, Route} from 'react-router-dom'
-import SignOut from './pages/Signup'
 import Signin from './pages/Signin'
 import Signup from './pages/Signup'
 import About from './pages/About'
 import Profile from './pages/Profile'
+import Home from './pages/Home'
+import Header from './components/Header.jsx'
 
 const App = () => {
-  return 
+  return (
   <BrowserRouter>
+   <Header/>
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path="/" element={<Home/>} />
       <Route path="/sign-up" element={<Signup/>} />
       <Route path="/sign-in" element={<Signin/>} />
       <Route path="/about" element={<About/>} />
       <Route path="/profile" element={<Profile/>} />
     </Routes>
   </BrowserRouter>
+  )
 }
 export default App
--- server/index.js
+++ server/index.js
@@ -16,7 +16,7 @@ app.use(express.json());
 
 
 app.listen(3000, () => {
-    console.log('Server is running on port 3000!');
+    console.log("Server is running on port 3001");
   }); 
 
 app.use('/api/user', userRouter);
